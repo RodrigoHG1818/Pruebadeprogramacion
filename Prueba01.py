66a47af3516df874d3890e2e284c4aeacff48260
RodrigoHG1818 <117099074+RodrigoHG1818@users.noreply.github.com>
2022-11-06 17:07:08 -0500

    Add files via upload

---
+++ Prueba01.py
@@ -0,0 +1,5 @@
+a= int(input("Ingresa el numero de mujeres: "))
+b= int(input("Ingresa el numero de hombres: "))
+total = a + b
+print("El porcentaje de mujeres es: ", (a/total)*100,"%")
+print("El porcentaje de hombres es: ", (b/total)*100,"%")
